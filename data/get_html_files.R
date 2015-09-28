10bcae7445d0a66ded023b2249d11009570761be
friep <friedrike.preu@gmail.com>
2015-09-28 12:13:26 +0200

    get html files, scrape pages and clean data

---
+++ data/get_html_files.R
@@ -0,0 +1,22 @@
+library(foreign)
+library(RCurl)
+library(stringr)
+
+#set working directory
+setwd('~/datadive')
+
+#load petitions
+petitions <- read.csv("data/liste_in_zeichnung.csv", header = T, stringsAsFactors = F)
+
+#create new directory
+dir.create("data/html_files")
+
+#download html files, create id variable
+for(i in 1:length(petitions$urls)){
+  content <- getURL(petitions$urls[i])
+  write(content, str_c("data/html_files/", i, ".html"))
+  petitions$id[i] <- i
+  Sys.sleep(0.2)
+}
+
+write.csv(petitions, "data/liste_in_zeichnung.csv")
