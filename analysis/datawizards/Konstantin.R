fb721e09c95124216a4abbd930c28eb27b48dd60
Konstantin Gavras <konstantin@gavras.de>
2015-10-18 16:08:52 +0200

    Daten sauber eingelesen

--- analysis/datawizards/Konstantin.R
+++ analysis/datawizards/Konstantin.R
@@ -1,8 +1,31 @@
 #DataDive
 #Gruppe Openpetition Data
 
+load("C:/Users/CorrealAid/Documents/GitHub/datadive/data/openpetition/cnames.RData")
+
 setwd("G:/openpetition")
 
 #Öffne alle CSV Dateien gleichzeitig
 temp = list.files(pattern="*.csv")
 for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i], header=F))
+
+clist <- c("argument", "email_forard_sent", "history", "petition", "question", "representative", "representative_response", "vote", "zeichnomat")
+
+colnames(argument.csv) <- cnames$argument
+colnames(email_forward_sent.csv) <- cnames$email_forward_sent
+colnames(history.csv) <- cnames$history
+colnames(petition.csv) <- cnames$petition
+colnames(question.csv) <- cnames$question
+colnames(representative.csv) <- cnames$representative
+colnames(representative_response.csv) <- cnames$representative_response
+
+setwd("G:/samples_op")
+
+load("G:/samples_op/signer_oht.RData")
+
+signer <- signer_data_oht
+
+colnames(signer) <- cnames$signer
+
+
+
